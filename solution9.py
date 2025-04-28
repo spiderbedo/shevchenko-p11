0dc2038233e94ebddb5d55f0d3d182aba625cfcf
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-04-28 15:02:48 +0700

    Update solution9.py

--- solution9.py
+++ solution9.py
@@ -4,7 +4,7 @@ import os
 os.makedirs('simple', exist_ok=True)
 
 with open('input.txt', 'r') as infile, \
-     open('simple/input.txt', 'w') as outfile:
+     open('simple/output.txt', 'w') as outfile:
     for idx, line in enumerate(infile, start=1):
         if idx % 2 == 0:
             outfile.write(line)
